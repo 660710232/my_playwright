8308ec4d16c67e49a8f64a60a588575df5c3f3f7
660710232 <phantawong_p@silpakorn.edu>
2026-02-23 13:25:59 +0700

    First Example Login Test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('test', async ({ page }) => {
+test("Login Test", async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-
-  const usernameInput = page.getByPlaceholder('Username');
-  const passwordInput = page.getByPlaceholder('Password');
-  const loginButton = page.getByRole('button', { name: 'Login'});
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
   
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+  await expect.soft(page.locator('.title')).toHaveText('Products');
+
+  await expect.soft(page.locator('.inventory_item')).toHaveCount(6);
 });
